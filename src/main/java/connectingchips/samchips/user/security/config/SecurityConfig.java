6496b6cf547202431a24aa162f25a05450b913d9
SBSun <46879319+SBSun@users.noreply.github.com>
2023-09-11 20:39:36 +0900

    Feat: SecurityConfig 생성 - h2, swagger 권한 없이 접속 가능하게 설정

---
+++ src/main/java/connectingchips/samchips/user/security/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package connectingchips.samchips.user.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        http.csrf(csrf -> csrf.disable());
+
+        // 세션을 사용하지 않기 때문에 STATELESS로 설정
+        http.sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // Spring Security 6.1.0부터는 메서드 체이닝의 사용을 지양하고 람다식을 통해 함수형으로 설정하게 지향
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers(
+                        new AntPathRequestMatcher("/"),
+                        new AntPathRequestMatcher("/h2/**"),
+                        new AntPathRequestMatcher("/h2-console/**"),
+                        new AntPathRequestMatcher("/swagger-ui/**"),
+                        new AntPathRequestMatcher("/error")
+                ).permitAll()
+                .anyRequest().authenticated()
+        );
+
+        http.headers((headers -> headers
+                .frameOptions(frameOptionsConfig ->
+                        frameOptionsConfig.disable()
+                )
+        ));
+
+        return http.build();
+    }
+}
