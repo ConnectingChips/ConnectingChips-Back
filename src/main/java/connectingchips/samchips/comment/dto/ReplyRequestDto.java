1f879deae93df6af35c952a70dd268967ae08f1a
bnfkim <kgr3819@gmail.com>
2023-09-12 20:19:10 +0900

    Feat: Reply Directory

---
+++ src/main/java/connectingchips/samchips/comment/dto/ReplyRequestDto.java
@@ -0,0 +1,4 @@
+package connectingchips.samchips.comment.dto;
+
+public class ReplyRequestDto {
+}
---
+++ src/main/java/connectingchips/samchips/comment/repository/ReplyRepository.java
@@ -0,0 +1,7 @@
+package connectingchips.samchips.comment.repository;
+
+import connectingchips.samchips.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository {
+}
