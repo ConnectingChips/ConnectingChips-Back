4ec702612139a2fc144dffe31ae80ffb2d535acc
bnfkim <kgr3819@gmail.com>
2023-09-15 23:39:41 +0900

    feat: Read 기능을 위한 dto

--- src/main/java/connectingchips/samchips/board/dto/BoardResponseDto.java
+++ src/main/java/connectingchips/samchips/board/dto/BoardResponseDto.java
@@ -7,6 +7,41 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BoardResponseDto {
 
+    /**
+     *    "status" : 200
+     *    "data": [
+     *         {
+     *             "id": 1, <- BoardId
+     *             "userId": 1,
+     *             "userName": "칩스",
+     *             "content": "내용",
+     *             "createDate": "2023년 09월 06일",
+     *             "likeCount": 16,
+     *             "canLike": true,
+     *             "image", "이미지",
+     *             "commentCount" : "4"
+     *             "commentList":[
+     *                   {
+     *                        "id": 2, <- CommentId
+     *                        "userId": 2,
+     *                        "userName": "프론트칩스",
+     *                        "content": "내용",
+     *                        "profileImage": "프로필 이미지",
+     *                        "createDate": "09월 07일 14:26"
+     *                        "replyList:"[
+     *                             {
+     *                                  "id": 3, <- ReplyId
+     *                                  "userId": 3,
+     *                                  "userName": "백엔드칩스",
+     *                                  "content": "내용",
+     *                                  "profileImage": "프로필 이미지"
+     *                                  "createDate": "09월 07일 14:26"
+     *                             },
+     */
+    public static class Read{
+
+    }
+
     public static class Update{
         private Long boardId;
         private String content;
