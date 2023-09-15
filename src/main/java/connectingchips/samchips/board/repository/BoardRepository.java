92c2331f9f1717142074faa00cc211c7b3d2e195
bnfkim <kgr3819@gmail.com>
2023-09-15 23:40:03 +0900

    feat: Read 기능을 위한 repository 추가

--- src/main/java/connectingchips/samchips/board/repository/BoardRepository.java
+++ src/main/java/connectingchips/samchips/board/repository/BoardRepository.java
@@ -4,6 +4,9 @@ import connectingchips.samchips.board.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findAllByMindId(Long mindId);
 }
