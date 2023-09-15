fca01c9fb5ab4286a0bdde086cd36c1921fd49ce
bnfkim <kgr3819@gmail.com>
2023-09-16 01:03:05 +0900

    fix: CommentResponseDto 수정

--- src/main/java/connectingchips/samchips/comment/dto/CommentResponseDto.java
+++ src/main/java/connectingchips/samchips/comment/dto/CommentResponseDto.java
@@ -9,31 +9,40 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
-    private Long id;
-    private Long boardId;
-    private Long userId;
-    private String nickname;
-    private String content;
-    private String profileImage;
-    private String createDate;
 
-    /* Entity -> Dto*/
-    public CommentResponseDto (User user, Board board, Comment comment){
-        this.id = comment.getId();
-        this.boardId = board.getBoardId();
-        this.userId = user.getId();
-        this.nickname = user.getNickname();
-        this.content = comment.getContent();
-        this.profileImage = user.getProfileImage();
-        // 09월 07일 14:2
-        StringBuilder sb = new StringBuilder();
-        sb.append(comment.getCreatedAt().getMonth()).append("월 ")
-                        .append(comment.getCreatedAt().getDayOfMonth()).append("일 ")
-                        .append(comment.getCreatedAt().getHour()).append(":").append(comment.getCreatedAt().getMinute());
-        this.createDate = sb.toString();
+    public static class ReturnList {
+        private int commentCount;
+        private List<Comment> commentList;
+    }
+
+    public static class Read {
+        private Long commentId;
+        private Long boardId;
+        private Long userId;
+        private String nickname;
+        private String content;
+        private String profileImage;
+        private String createDate;
+
+        /* Entity -> Dto*/
+        public Read (User user, Board board, Comment comment){
+            this.commentId = comment.getId();
+            this.boardId = board.getBoardId();
+            this.userId = user.getId();
+            this.nickname = user.getNickname();
+            this.content = comment.getContent();
+            this.profileImage = user.getProfileImage();
+            // 09월 07일 14:2
+            StringBuilder sb = new StringBuilder();
+            sb.append(comment.getCreatedAt().getMonth()).append("월 ")
+                    .append(comment.getCreatedAt().getDayOfMonth()).append("일 ")
+                    .append(comment.getCreatedAt().getHour()).append(":").append(comment.getCreatedAt().getMinute());
+            this.createDate = sb.toString();
+        }
     }
 }
