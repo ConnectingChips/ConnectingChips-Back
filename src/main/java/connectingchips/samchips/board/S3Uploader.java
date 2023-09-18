3cb24b7c58252499e5c3ae14870096116db0a957
bnfkim <kgr3819@gmail.com>
2023-09-18 19:20:16 +0900

    Feat : S3Uploader class 추가

--- src/main/java/connectingchips/samchips/board/S3Uploader.java
+++ src/main/java/connectingchips/samchips/board/S3Uploader.java
@@ -1,11 +1,10 @@
 package connectingchips.samchips.board;
 
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,9 +15,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Component
 public class S3Uploader {
-
     /**
      * [코드의 순서]
      * 1. MultipartFile을 전달
@@ -30,8 +27,7 @@ public class S3Uploader {
      *      - Multipartfile -> File로 전환되면서 로컬에 파일 생성된 것을 삭제
      * 5. 업로드된 파일의 S3 URL 주소를 반환
      */
-
-    private final AmazonS3Client amazonS3Client;
+    private final AmazonS3 amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
@@ -39,7 +35,6 @@ public class S3Uploader {
     //로컬 프로젝트에 사진 파일이 생성되지만, removeNewFile()을 통해서 바로 지워줄 예정
     private Optional<File> convert(MultipartFile file) throws IOException {
         File convertFile = new File(file.getOriginalFilename());
-
         if(convertFile.createNewFile()) {
             try (FileOutputStream fos = new FileOutputStream(convertFile)) {
                 fos.write(file.getBytes());
@@ -57,15 +52,14 @@ public class S3Uploader {
     }
 
     private void removeNewFile(File file) {
-        if(file.delete()) System.out.println("파일삭제성공");
-        else System.out.println("파일삭제실패");
+        if(file.delete()) System.out.println("파일 삭제 성공");
+        else System.out.println("파일 삭제 실패");
     }
 
     //MultipartFile을 전달받아 File로 전환 후 S3에 업로드
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
         File uploadFile = convert(multipartFile)
                 .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
-
         return upload(uploadFile, dirName);
     }
 
