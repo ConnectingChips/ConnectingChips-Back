32cbde98f7ec2ecdb0b08686fe0978b138488ee6
bnfkim <kgr3819@gmail.com>
2023-09-18 19:17:44 +0900

    Fix : image url 을 따로 받기 위해 dto 수정

--- src/main/java/connectingchips/samchips/board/dto/BoardRequestDto.java
+++ src/main/java/connectingchips/samchips/board/dto/BoardRequestDto.java
@@ -6,14 +6,9 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class BoardRequestDto {
-    /**
-     *     "mindId": 2,
-     *     "content": "내용",
-     *     "image": "인증 이미지"
-     */
+
     private Long mindId;
     private Long userId;
     private String content;
-    private String image;
 
 }
