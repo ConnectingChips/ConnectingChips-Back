3e237e1f1c141c80662b13f303c1433a0dca4b3c
bnfkim <kgr3819@gmail.com>
2023-09-18 17:04:53 +0900

    Feat: 파일받아 서버로 업로드 하는 클래스

---
+++ src/main/java/connectingchips/samchips/board/service/S3Uploader.java
@@ -0,0 +1,69 @@
+package connectingchips.samchips.board.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+@Service
+public class S3Uploader {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    //로컬 프로젝트에 사진 파일이 생성되지만, removeNewFile()을 통해서 바로 지워줄 예정
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+
+        if(convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+        return Optional.empty();
+    }
+    private String putS3(File uploadfile, String fileName){
+        amazonS3Client.putObject(
+                new PutObjectRequest(bucket, fileName, uploadfile)
+                        .withCannedAcl(CannedAccessControlList.PublicRead)
+        );
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private void removeNewFile(File file) {
+        if(file.delete()) System.out.println("파일삭제성공");
+        else System.out.println("파일삭제실패");
+    }
+
+    //MultipartFile을 전달받아 File로 전환 후 S3에 업로드
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
+
+        return upload(uploadFile, dirName);
+    }
+
+    public String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+
+        //로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
+        removeNewFile(uploadFile);
+
+        return uploadImageUrl; //업로드된 파일의 S3 URL 주소 반환
+    }
+}
