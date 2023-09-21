9cb3fb2dfdee453ec77ebd3de3a3da659b4823fa
HeWillGoTillTheEnd <kingofunder@gmail.com>
2023-09-21 16:34:50 +0900

    Fix: JoinCheckResponse 변경

--- src/main/java/connectingchips/samchips/joinedmind/dto/JoinCheckResponse.java
+++ src/main/java/connectingchips/samchips/joinedmind/dto/JoinCheckResponse.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class JoinCheckResponse {
 
-    private final Boolean isDoneToday;
+    private final Boolean isJoining;
 
     public static JoinCheckResponse of(boolean isJoining){
         return new JoinCheckResponse(isJoining);
