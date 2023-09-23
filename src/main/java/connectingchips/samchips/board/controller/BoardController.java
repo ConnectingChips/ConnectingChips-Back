27d1c56638ba78fee014e8d08768926f2fa1da5a
bnfkim <kgr3819@gmail.com>
2023-09-23 10:19:27 +0900

    fix: private -> public method

--- src/main/java/connectingchips/samchips/board/controller/BoardController.java
+++ src/main/java/connectingchips/samchips/board/controller/BoardController.java
@@ -5,10 +5,8 @@ import connectingchips.samchips.board.dto.BoardResponseDto;
 import connectingchips.samchips.board.service.BoardService;
 import connectingchips.samchips.commons.dto.BasicResponse;
 import connectingchips.samchips.commons.dto.DataResponse;
-import connectingchips.samchips.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,14 +22,14 @@ public class BoardController {
 
     /* 게시글 리스트 반환 */
     @GetMapping("/{mind_id}")
-    private DataResponse<List<BoardResponseDto.Read>> getMindBoardList(@PathVariable(value = "mind_id") Long mindId){
+    public DataResponse<List<BoardResponseDto.Read>> getMindBoardList(@PathVariable(value = "mind_id") Long mindId){
         List<BoardResponseDto.Read> mindBoardList = boardService.getMindBoardList(mindId);
         return DataResponse.of(mindBoardList);
     }
 
     /* 게시글 작성자 여부 */
     @GetMapping("/authentication")
-    private DataResponse<BoardResponseDto.CanEdit> getBoardUser(@RequestParam(value = "board_id") Long boardId,
+    public DataResponse<BoardResponseDto.CanEdit> getBoardUser(@RequestParam(value = "board_id") Long boardId,
                                                                 @RequestParam(value = "user_id") Long userId){
         BoardResponseDto.CanEdit canEdit = boardService.isUserEditor(boardId, userId);
         return DataResponse.of(canEdit);
@@ -39,7 +37,7 @@ public class BoardController {
 
     /* 게시글 작성 */
     @PostMapping
-    private BasicResponse createBoard(@RequestPart(value = "file", required = false) MultipartFile file,
+    public BasicResponse createBoard(@RequestPart(value = "file", required = false) MultipartFile file,
                                       @RequestPart(value = "boardRequestDto") BoardRequestDto.Save boardRequestDto) throws IOException {
         boardService.createBoard(file, boardRequestDto);
 
@@ -49,14 +47,14 @@ public class BoardController {
     /* 게시글 수정 */
     @PutMapping("/{board_id}")
     @CrossOrigin(origins = "http://localhost:3000", methods = RequestMethod.PUT) //특정 url에 cors에러 해결
-    private DataResponse<BoardResponseDto.Update> updateBoard(@PathVariable(value = "board_id") Long boardId, @RequestBody BoardRequestDto.Edit boardRequestDto) {
+    public DataResponse<BoardResponseDto.Update> updateBoard(@PathVariable(value = "board_id") Long boardId, @RequestBody BoardRequestDto.Edit boardRequestDto) {
         BoardResponseDto.Update boardResponseDto = boardService.updateBoard(boardId, boardRequestDto);
         return DataResponse.of(boardResponseDto);
     }
 
     /* 게시글 삭제 */
     @DeleteMapping("/{board_id}")
-    private BasicResponse deleteBoard(@PathVariable(value = "board_id") Long boardId){
+    public BasicResponse deleteBoard(@PathVariable(value = "board_id") Long boardId){
         boardService.deleteBoard(boardId);
         return BasicResponse.of(HttpStatus.OK);
     }
