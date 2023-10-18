683dbed87c484a367922075719237446337887ba
bnfkim <kgr3819@gmail.com>
2023-10-18 17:16:55 +0900

    refactor: file Upload refactor - 한 메소드에 한 기능만 할 수 있도록

--- src/main/java/connectingchips/samchips/board/S3Uploader.java
+++ src/main/java/connectingchips/samchips/board/S3Uploader.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
@@ -23,51 +22,66 @@ import static connectingchips.samchips.global.exception.CommonErrorCode.*;
 @RequiredArgsConstructor
 @Slf4j
 public class S3Uploader {
-    /**
-     * [코드의 순서]
-     * 1. MultipartFile을 전달
-     * 2. S3에 전달할 수 있도록 MultiPartFile을 File로 전환
-     *      - S3에 Multipartfile 타입은 전송이 안 됨
-     * 3. 전환된 File을 S3에 public 읽기 권한으로 put
-     *      - 외부에서 정적 파일을 읽을 수 있도록 하기 위함
-     * 4. 로컬에 생성된 File 삭제
-     *      - Multipartfile -> File로 전환되면서 로컬에 파일 생성된 것을 삭제
-     * 5. 업로드된 파일의 S3 URL 주소를 반환
-     */
+
     private final AmazonS3 amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    /**
-     * S3에 파일 업로드
-     * @param uploadfile : 업로드할 파일
-     * @param fileName : 업로드할 파일 이름
-     * @return : 업로드 경로
-     */
-    private String putS3(File uploadfile, String fileName){
-        amazonS3Client.putObject(
-                new PutObjectRequest(bucket, fileName, uploadfile)
+    public String uploadFile(MultipartFile multipartFile, String filePath) throws IOException {
+        File uploadFile = convertMultipartFileToFile(multipartFile)
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_FILE));
+        String uploadS3fileName = creatUniqueFileName(uploadFile.getName());
+        String fileName = filePath + "/" + uploadS3fileName;
+        uploadS3Bucket(uploadFile, fileName);
+        deleteLocalFile(uploadFile);
+
+        return getFileUrl(fileName);
+    }
+
+    //MultiPartFile -> File로 전환
+    private Optional<File> convertMultipartFileToFile(MultipartFile multipartFile) throws IOException {
+        File file = new File(multipartFile.getOriginalFilename());
+        multipartFile.transferTo(file);
+        return file.exists() ? Optional.of(file) : Optional.empty();
+    }
+
+    //S3 파일 업로드
+    private void uploadS3Bucket(File uploadFile, String fileName){
+        amazonS3Client.putObject( new PutObjectRequest(bucket, fileName, uploadFile)
                         .withCannedAcl(CannedAccessControlList.PublicRead)
         );
+    }
+
+    //로컬 파일 삭제
+    private void deleteLocalFile(File file) {
+        if(file.delete()) return;
+        else throw new RestApiException(INVALID_REQUEST);
+    }
+
+    //파일 URL 반환
+    public String getFileUrl(String fileName){
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
-    /**
-     * S3에 파일 삭제
-     */
-    public void deleteS3(String filePath) throws Exception {
-        try {
-            amazonS3Client.deleteObject(bucket, filePath);
-        } catch (AmazonServiceException e) {
-            throw new RestApiException(INVALID_REQUEST);
-        }
+    //기존 확장자 명을 유지한 채, UUID 파일 생성
+    private String creatUniqueFileName(String originalFileName) {
+        String extension = getFileExtension(originalFileName);
+        //이미지 확장자 제한하기 위한 코드
+        if (!isImageExtension(extension)) throw new BadRequestException(INVALID_REQUEST);
+        String uniqueName = UUID.randomUUID().toString();
+        return uniqueName + "." + extension;
     }
 
-    //로컬에 있는 파일 삭제
-    private void removeLocalFile(File file) {
-        if(file.delete()) System.out.println("파일 삭제 성공");
-        else System.out.println("파일 삭제 실패");
+    //확장자 추출
+    private String getFileExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+
+    //파일 유효성 검사 (이미지 확장자만 업로드 가능)
+    private boolean isImageExtension(String extension) {
+        String[] imageExtensions = {"jpg", "jpeg", "png", "heic", "bmp"};
+        return Arrays.stream(imageExtensions).anyMatch(ext -> ext.equalsIgnoreCase(extension));
     }
 
     public List<String> find(String prefix){
@@ -95,41 +109,13 @@ public class S3Uploader {
         return fileNames;
     }
 
-    public String getFileUrl(String fileName){
-        return amazonS3Client.getUrl(bucket, fileName).toString();
-    }
-
-    public String upload(MultipartFile multipartFile, String filePath) throws IOException {
-        File uploadFile = convert(multipartFile)
-                .orElseThrow(() -> new BadRequestException(NOT_FOUND_FILE));
-        return upload(uploadFile, filePath);
-    }
-
-    /**
-     * MultiPartFile -> File로 전환
-     */
-    private Optional<File> convert(MultipartFile file) throws IOException {
-        File convertFile = new File(file.getOriginalFilename());
-        if(convertFile.createNewFile()) {
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-                fos.write(file.getBytes());
-            }
-            return Optional.of(convertFile);
+    //S3 파일 삭제
+    public void deleteS3File(String filePath) throws Exception {
+        try {
+            amazonS3Client.deleteObject(bucket, filePath);
+        } catch (AmazonServiceException e) {
+            throw new RestApiException(INVALID_REQUEST);
         }
-        return Optional.empty();
-    }
-
-    public String upload(File uploadFile, String dirName) {
-        //set UUID + 확장자 제한
-        String uploadS3fileName = createFileName(uploadFile.getName());
-        //set Directory
-        String fileName = dirName + "/" + uploadS3fileName;
-        //s3에 업로드
-        String uploadImageUrl = putS3(uploadFile, fileName);
-        //로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
-        removeLocalFile(uploadFile);
-
-        return uploadImageUrl; //업로드된 파일의 S3 URL 주소 반환
     }
 
     //================================== 추가한 부분=========================
@@ -155,27 +141,5 @@ public class S3Uploader {
             }
         }
         return imgUrlList;
-    }
-
-    //기존 확장자 명을 유지한 채, UUID 파일 생성
-    private String createFileName(String originalFileName) {
-        String extension = getFileExtension(originalFileName);
-        if (!isImageExtension(extension)) {
-            throw new BadRequestException(INVALID_REQUEST);
-        }
-
-        String uniqueName = UUID.randomUUID().toString();
-        return uniqueName + "." + extension;
-    }
-
-    //확장자 추출
-    private String getFileExtension(String fileName) {
-        return fileName.substring(fileName.lastIndexOf(".") + 1);
-    }
-
-    //파일 유효성 검사 (이미지 확장자만 업로드 가능)
-    private boolean isImageExtension(String extension) {
-        String[] imageExtensions = {"jpg", "jpeg", "png", "heic", "bmp"};
-        return Arrays.stream(imageExtensions).anyMatch(ext -> ext.equalsIgnoreCase(extension));
     }
 }
