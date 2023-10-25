6771373184ade45585c432a243c7dcf8c5de0a8b
HeWillGoTillTheEnd <kingofunder@gmail.com>
2023-10-25 11:54:46 +0900

    Feat: Test CD

--- src/test/java/connectingchips/samchips/SamchipsApplicationTests.java
+++ src/test/java/connectingchips/samchips/SamchipsApplicationTests.java
@@ -2,11 +2,13 @@ package connectingchips.samchips;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootTest
 class SamchipsApplicationTests {
 
 	@Test
+	@Profile("prod")
 	void contextLoads() {
 	}
 
