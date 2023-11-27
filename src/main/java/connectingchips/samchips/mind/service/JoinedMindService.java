312f75b5832ac5edf76ca445caa71d695aa5795e
bnfkim <kgr3819@gmail.com>
2023-11-27 23:23:05 +0900

    refactor: 불필요한 JPA SAVE 메소드 제거

--- src/main/java/connectingchips/samchips/mind/service/JoinedMindService.java
+++ src/main/java/connectingchips/samchips/mind/service/JoinedMindService.java
@@ -52,9 +52,19 @@ public class JoinedMindService {
             return;
         }
 
-        //재작심인 경우 -> 유효성 확인 후 재 연결
-        JoinedMind beforeJoinedMind = findVerifiedJoinedMindByIsJoining(mindId, joinedMinds, NOT_JOIN); //예전 참여했던 작심 가져오기
-        makeReJoinedMind(joinedMinds, beforeJoinedMind, user);
+        JoinedMind beforeJoinedMind = findJoinedMindByIsJoining(mindId, joinedMinds, NOT_JOIN); //예전 참여했던 작심 가져오기
+        beforeJoinedMind.updateIsJoining(JOIN);
+        //makeJoinedMindRelation(beforeJoinedMind);
+    }
+
+    @Transactional
+    public void exitMindRelation(Long mindId, User user) {
+        List<JoinedMind> joinedMinds = getJoinedMinds(user);
+        JoinedMind nowJoinedMind = findJoinedMindByIsJoining(mindId, joinedMinds, JOIN); //참여 중인 작심 가져옴
+        //(1) joinedMind 변경 (참여 중이지 않은 작심으로 변경)
+        nowJoinedMind.updateIsJoining(NOT_JOIN);
+        //(3) 변경된 JoinedMinds 를 User Entity 에 새로 업데이트 후 저장
+        //user.updateJoinedMinds(joinedMinds);
     }
 
     //user 엔티티에 있는 joinedMinds 를 유효성 검사 후 반환
@@ -69,16 +79,13 @@ public class JoinedMindService {
     }
 
     @Transactional
-    public void makeReJoinedMind(List<JoinedMind> joinedMinds, JoinedMind beforeJoinedMind, User user) {
-        //재작심하기 위해 해줘야하는 것
+    public void makeJoinedMindRelation(JoinedMind beforeJoinedMind) {
         //(1) Joining 변경 (2) 해당 User의 JoinedMind로 변경
         beforeJoinedMind.updateIsJoining(JOIN);
-        joinedMinds.add(beforeJoinedMind);
-        user.updateJoinedMinds(joinedMinds);
-        userRepository.save(user);
+        //user.updateJoinedMinds(joinedMinds);
     }
 
-    private JoinedMind findVerifiedJoinedMindByIsJoining(Long mindId, List<JoinedMind> joinedMinds, int joining) {
+    private JoinedMind findJoinedMindByIsJoining(Long mindId, List<JoinedMind> joinedMinds, int joining) {
         return joinedMinds.stream()
                 .filter(joinedMind -> joinedMind.getJoinedMindId().equals(mindId) && isJoining(joinedMind, joining))
                 .findFirst()
@@ -89,40 +96,11 @@ public class JoinedMindService {
         return joinedMind.getIsJoining().equals(joining);
     }
 
-    //현재 참여하고 있는지 확인
-    private JoinedMind checkUserHaveJoinedMind(Long mindId, User user) {
-        return user.getJoinedMinds()
-                .stream()
-                .filter(jm -> Objects.equals(jm.getMind().getMindId(), mindId) && jm.getIsJoining() == JOIN)
-                .findFirst()
-                .orElseThrow(() -> new BadRequestException(NOT_JOIN_MIND));
-    }
-
     public JoinedMind createJoinedMind(Long mindId, User user) {
         Mind mind = findVerifiedMind(mindId);
         JoinedMind joinedMind = JoinedMind.builder().mind(mind).user(user).build();
         return joinedMindRepository.save(joinedMind);
     }
-
-    @Transactional
-    public void exitMindRelation(Long mindId, User user) {
-        List<JoinedMind> joinedMinds = getJoinedMinds(user);
-        JoinedMind joinedMind = findVerifiedJoinedMindByIsJoining(mindId, joinedMinds, JOIN); //참여 중인 작심 가져옴
-
-        //(1) joinedMind 변경 (참여 중이지 않은 작심으로 변경)
-        joinedMind.updateIsJoining(NOT_JOIN);
-
-        //(2) NOT_JOIN으로 바꾼 작심을 새로 저장하고, JOIN 으로 되어있던 작심은 새로 변경 후 JoinedMind 에 새로운 데이터 저장
-        joinedMinds.add(joinedMind);
-        joinedMinds.remove(joinedMind); //기존에 있던 작심 제외
-        joinedMindRepository.save(joinedMind);
-
-        //(3) 변경된 JoinedMinds 를 User Entity 에 새로 업데이트 후 저장
-        user.updateJoinedMinds(joinedMinds);
-        userRepository.save(user);
-    }
-
-
 
     private Mind findVerifiedMind(Long mindId) {
         return mindRepository.findById(mindId)
@@ -130,12 +108,12 @@ public class JoinedMindService {
     }
 
     public void updateKeepJoin(Long mindId, User user) {
-        JoinedMind joinedMind = checkUserHaveJoinedMind(mindId, user);
+        List<JoinedMind> joinedMinds = getJoinedMinds(user);
+        JoinedMind joinedMind = findJoinedMindByIsJoining(mindId, joinedMinds, JOIN);
 
         if(!joinedMind.getKeepJoin()) throw new BadRequestException(INVALID_REQUEST);
 
         joinedMind.setKeepJoin(false);
-        joinedMindRepository.save(joinedMind);
     }
     public void resetCountAndUpdateKeepJoin(){
         joinedMindRepositoryImpl.resetCountAndUpdateKeepJoin();
