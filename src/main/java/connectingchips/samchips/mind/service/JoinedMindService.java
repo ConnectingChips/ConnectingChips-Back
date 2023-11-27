19729077110294fe94addf278bc524982d21f977
bnfkim <kgr3819@gmail.com>
2023-11-27 23:04:01 +0900

    refactor: isJoining method 분리

--- src/main/java/connectingchips/samchips/mind/service/JoinedMindService.java
+++ src/main/java/connectingchips/samchips/mind/service/JoinedMindService.java
@@ -53,10 +53,11 @@ public class JoinedMindService {
         }
 
         //재작심인 경우 -> 유효성 확인 후 재 연결
-        JoinedMind beforeJoinedMind = findVerifiedJoinedMind(mindId, joinedMinds, NOT_JOIN); //예전 참여했던 작심 가져오기
+        JoinedMind beforeJoinedMind = findVerifiedJoinedMindByIsJoining(mindId, joinedMinds, NOT_JOIN); //예전 참여했던 작심 가져오기
         makeReJoinedMind(joinedMinds, beforeJoinedMind, user);
     }
 
+    //user 엔티티에 있는 joinedMinds 를 유효성 검사 후 반환
     private List<JoinedMind> getJoinedMinds(User user) {
         List<JoinedMind> joinedMinds = user.getJoinedMinds();
         validateJoinedMind.validateJoinedMinds(joinedMinds, user);
@@ -77,11 +78,24 @@ public class JoinedMindService {
         userRepository.save(user);
     }
 
-    private JoinedMind findVerifiedJoinedMind(Long mindId, List<JoinedMind> joinedMinds, int IsJoining) {
+    private JoinedMind findVerifiedJoinedMindByIsJoining(Long mindId, List<JoinedMind> joinedMinds, int joining) {
         return joinedMinds.stream()
-                .filter(joinedMind -> joinedMind.getJoinedMindId().equals(mindId) && joinedMind.getIsJoining().equals(IsJoining))
+                .filter(joinedMind -> joinedMind.getJoinedMindId().equals(mindId) && isJoining(joinedMind, joining))
                 .findFirst()
                 .orElseThrow(() -> new BadRequestException(ALREADY_JOIN_MIND));
+    }
+
+    private boolean isJoining(JoinedMind joinedMind, int joining) {
+        return joinedMind.getIsJoining().equals(joining);
+    }
+
+    //현재 참여하고 있는지 확인
+    private JoinedMind checkUserHaveJoinedMind(Long mindId, User user) {
+        return user.getJoinedMinds()
+                .stream()
+                .filter(jm -> Objects.equals(jm.getMind().getMindId(), mindId) && jm.getIsJoining() == JOIN)
+                .findFirst()
+                .orElseThrow(() -> new BadRequestException(NOT_JOIN_MIND));
     }
 
     public JoinedMind createJoinedMind(Long mindId, User user) {
@@ -93,25 +107,22 @@ public class JoinedMindService {
     @Transactional
     public void exitMindRelation(Long mindId, User user) {
         List<JoinedMind> joinedMinds = getJoinedMinds(user);
-        JoinedMind joinedMind = findVerifiedJoinedMind(mindId, joinedMinds, JOIN);
+        JoinedMind joinedMind = findVerifiedJoinedMindByIsJoining(mindId, joinedMinds, JOIN); //참여 중인 작심 가져옴
 
-        joinedMinds.remove(joinedMind);
+        //(1) joinedMind 변경 (참여 중이지 않은 작심으로 변경)
         joinedMind.updateIsJoining(NOT_JOIN);
 
+        //(2) NOT_JOIN으로 바꾼 작심을 새로 저장하고, JOIN 으로 되어있던 작심은 새로 변경 후 JoinedMind 에 새로운 데이터 저장
         joinedMinds.add(joinedMind);
-        user.updateJoinedMinds(joinedMinds);
+        joinedMinds.remove(joinedMind); //기존에 있던 작심 제외
         joinedMindRepository.save(joinedMind);
+
+        //(3) 변경된 JoinedMinds 를 User Entity 에 새로 업데이트 후 저장
+        user.updateJoinedMinds(joinedMinds);
         userRepository.save(user);
     }
 
-    //현재 참여하고 있는지 확인
-    private JoinedMind checkUserHaveJoinedMind(Long mindId, User user) {
-        return user.getJoinedMinds()
-                .stream()
-                .filter(jm -> Objects.equals(jm.getMind().getMindId(), mindId) && jm.getIsJoining() == JOIN)
-                .findFirst()
-                .orElseThrow(() -> new BadRequestException(NOT_JOIN_MIND));
-    }
+
 
     private Mind findVerifiedMind(Long mindId) {
         return mindRepository.findById(mindId)
